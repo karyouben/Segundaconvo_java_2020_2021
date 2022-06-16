71006c7a4cecbe7bfe7399e59a5fad721ee9c30c
willi <willi@192.168.1.131>
2022-06-16 20:18:13 +0200

    16/06/2022

--- src/fp/RegistroActividad/RegistrosUsuarios.java
+++ src/fp/RegistroActividad/RegistrosUsuarios.java
@@ -4,6 +4,12 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.SortedMap;
+import java.util.TreeMap;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -29,6 +35,7 @@ public class RegistrosUsuarios {
 		this.registros=new ArrayList<RegistroActividad>(registros);
 	}
 	
+	//EJERCICIO 1
 	public List<String> usuariosConActividadEnPuntoConDistanciaMenorOIgual(Coordenada c,Double umbral){
 		return registros.stream()
 				.filter(r->r.getCoordenadaPuntoRutaMasCercanoA(c, umbral)!=null)
@@ -37,6 +44,54 @@ public class RegistrosUsuarios {
 				.distinct()
 				.collect(Collectors.toList());
 	}
+	//EJERCICIO 2
+	
+//	public SortedMap<Integer,Long> totalDeMinutosPorActividad(String usuario){
+//		return registros.stream()
+//				.filter(r->r.getUsuario().equals(usuario))
+//				.collect(Collectors.groupingBy(r->UtilesFechas.getWeekOfYear(r.fecha()),
+//						TreeMap::new,Collectors.summingLong(r->r.duracion().toMinutes())));
+		
+//	}
+	//EJERCICIO 3
+	
+	public TipoActividad getTipoActividadMasPracticada() {
+		Map<TipoActividad,Integer> m=numDeUsuariosPorActividad2();
+		Comparator<Map.Entry<TipoActividad, Integer>> c=Comparator.comparing(Map.Entry::getValue);
+		return m.entrySet().stream()
+				.max(c)
+				.get()
+				.getKey();
+		
+	}
+	
+	private Map<TipoActividad,Set<String>> numDeUsuariosPorActividad(){
+		return registros.stream()
+				.collect(Collectors.groupingBy(RegistroActividad::getTipo,
+						Collectors.mapping(RegistroActividad::getUsuario, Collectors.toSet())));
+	}
+	
+	private Map<TipoActividad,Integer> numDeUsuariosPorActividad2(){
+		Map<TipoActividad,Set<String>> m=numDeUsuariosPorActividad();
+		return m.entrySet().stream()
+				.collect(Collectors.toMap(c->c.getKey(), c->c.getValue().size()));
+	}
+	
+	//Ejercicio 4
+	
+//	public Boolean esUsuarioSaludableOMS(String usuario) {
+//		SortedMap<Integer,Long> actividad=totalDeMinutosPorActividad(usuario);
+//		SortedMap<Integer,Long> numActividades=totalDeMinutosPorActividad(usuario);
+//		
+//		return actividad.size()>=48 && actividad.entrySet().stream().allMatch(c->c.getValue()>=180L && numActividades.get(c.getKey())>=5L);
+//		
+//	}
+	
+//	public SortedMap<Integer,Long> getNumActividadesPorSemana(String usuario){
+//		return registros.stream()
+//				.filter(r->r.getUsuario().equals(usuario))
+//				.collect(Collectors.groupingBy(r->UtilesFechas.getWeekOfYear(r.fecha()),TreeMap::new,Collectors.counting()));
+//	}
 
 }
 
---
+++ src/fp/TestRegistroActividad/TestFactoriaRegistrosActividades.java
@@ -0,0 +1,10 @@
+package fp.TestRegistroActividad;
+
+public class TestFactoriaRegistrosActividades {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
